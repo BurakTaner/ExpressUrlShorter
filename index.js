f225e2dc7746f2511272093a2c6cab1329e49356
Ertan Taner <ertantaner2000@gmail.com>
2022-11-29 01:47:46 +0300

    feat: URL shorter added

--- index.js
+++ index.js
@@ -2,13 +2,18 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const bodyParser = require('body-parser');
+
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 
+app.use(bodyParser.urlencoded({extended: false}));
+
 app.use('/public', express.static(`${process.cwd()}/public`));
+
 
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
@@ -18,6 +23,56 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+const urlArr = [];
+let key = 1;
+
+
+app.route("/api/shorturl/:id?")
+.post((req,res,next) => {
+  let {url: userUrl} = req.body;
+
+  const urlRegex = /(https:\/\/|http:\/\/)www\.(\w+)\.(com|org|io)/
+
+  let regexResult = urlRegex.test(userUrl);
+
+
+  if(regexResult) {
+  let result = urlArr.find(elem => elem.long_url == userUrl);
+  
+  if(result === undefined) {
+    urlArr.push({
+      long_url: userUrl,
+      short_url: key
+    });
+    key++;
+    res.json({
+      long_url: userUrl,
+      short_url: key -1 
+    });
+  }
+
+  res.json({
+    long_url: `${result.long_url}`,
+    short_url: `${result.short_url}`
+  });
+  }
+  res.json({
+    error: "Invalid url"
+  });
+
+  next();
+})
+.get((req,res) => {
+  
+  let redirectURL = urlArr.find(elem => elem.short_url == req.params.id);
+
+  res.redirect(redirectURL.long_url);
+});
+
+
+
+
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
